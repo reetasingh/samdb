5a675f9266ee0b39a9b35e8a58f36e336bc2cf6a
Reeta Singh <singh.reeta66@gmail.com>
2024-10-05 12:30:08 -0700

    add image for understanding epoll

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module samdb
 
 go 1.23
+
+require golang.org/x/sys v0.25.0
---
+++ pkg/server/async_connect_goroutine.go
@@ -0,0 +1,46 @@
+// package server
+
+// import (
+// 	"fmt"
+// 	"net"
+// 	"samdb/pkg/core"
+// )
+
+// func AsyncTCPConnect(port int) error {
+// 	laddr := net.TCPAddr{Port: port}
+// 	listener, err := net.ListenTCP("tcp", &laddr)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	connections := 0
+// 	for {
+
+// 		conn, err := listener.Accept()
+// 		if err != nil {
+// 			return nil
+// 		}
+// 		connections = connections + 1
+// 		fmt.Printf("client connected: %v\n", conn.RemoteAddr())
+// 		fmt.Printf("concurrent connections: %d", connections)
+
+// 		for {
+// 			cmd, err := core.ReadCmd(conn)
+// 			if err != nil {
+// 				conn.Close()
+// 				break
+// 			}
+// 			fmt.Println("Received cmd: ", cmd.Cmd, cmd.Args)
+// 			result, err := core.ProcessCmd(cmd)
+// 			if err != nil {
+// 				_, err = conn.Write(core.EncodeError(err))
+// 				if err != nil {
+// 					conn.Close()
+// 				}
+// 			}
+// 			_, err = conn.Write(core.EncodeString(result))
+// 			if err != nil {
+// 				conn.Close()
+// 			}
+// 		}
+// 	}
+// }
